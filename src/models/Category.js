4a2131947e004250dba5b15331a7c888c93d157f
Diego Gomez <diegofelipegogo03@gmail.com>
2024-08-21 23:17:00 -0500

    Add category controller

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+const bcrypt = require("bcrypt")
+
+const Category = sequelize.define('category', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+});
+
+Category.beforeCreate(async (category) => {
+    const password = category.password
+    const hashPassword = await bcrypt.hash(password, 10)
+    category.password = hashPassword
+})
+
+module.exports = Category;

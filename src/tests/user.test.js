27f10ac5067167332728f764e18390df582adb1f
Diego Gomez <diegofelipegogo03@gmail.com>
2024-08-21 22:17:29 -0500

    Add Put and Delete tests

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -39,10 +39,10 @@ test("POST -> BASE_URL, should return statusCode 201, and res.body.firstName ===
         .post(BASE_URL)
         .send(user)
 
+    userId = res.body.id
+
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
-
-
 
     expect(res.body.firstName).toBeDefined()
     expect(res.body.firstName).toBe(user.firstName)
@@ -61,6 +61,7 @@ test("GET -> BASE_URL, should return statusCode 200, and res.body.length === 2",
     expect(res.body).toHaveLength(2)
 })
 
+
 test("POST -> 'BASE_URL/Login', should return status code 200, and res.body.user.email === user.email", async() => {
     const hits = {
         email: "iuvil@gmail.com",
@@ -78,35 +79,53 @@ test("POST -> 'BASE_URL/Login', should return status code 200, and res.body.user
     expect(res.body.user.email).toBe(hits.email)
 
 
+})
+
+
+
+test("POST -> 'BASE_URL/Login', should return status code 401", async() => {
+    const hits = {
+        email: "iuvil@gmail.com",
+        password: "invalidPassword"
+    }
+
+    const res = await request(app)
+        .post(`${BASE_URL}/login`)
+        .send(hits)
+
+    expect(res.statusCode).toBe(401)
+
 
 })
 
 
+
 // // PUT - Actualizar un usuario existente
-// test("PUT -> BASE_URL/:id, should return statusCode 200, and res.body.firstName === updatedUser.firstName", async () => {
-//     const updatedUser = {
-//         firstName: "UpdatedIuvil",
-//         lastName: "UpdatedPena",
-//         email: "updated.iuvil@gmail.com",
-//         phone: "+575312324"
-//     }
+test("PUT -> BASE_URL/:id, should return statusCode 200, and res.body.firstName === updatedUser.firstName", async () => {
+    const updatedUser = {
+        firstName: "UpdatedIuvil"
+    }
 
-//     const res = await request(app)
-//         .put(`${BASE_URL}/${userId}`)
-//         .set('Authorization', `Bearer ${TOKEN}`)
-//         .send(updatedUser)
+    const res = await request(app)
+        .put(`${BASE_URL}/${userId}`)
+        
+        .send(updatedUser)
+        .set('Authorization', `Bearer ${TOKEN}`)
 
-//     expect(res.statusCode).toBe(200)
-//     expect(res.body).toBeDefined()
-//     expect(res.body.firstName).toBe(updatedUser.firstName)
-//     expect(res.body.email).toBe(updatedUser.email)
-// })
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(updatedUser.firstName)
 
-// // DELETE - Eliminar un usuario existente
-// test("DELETE -> BASE_URL/:id, should return statusCode 204", async () => {
-//     const res = await request(app)
-//         .delete(`${BASE_URL}/${userId}`)
-//         .set('Authorization', `Bearer ${TOKEN}`)
+})
 
-//     expect(res.statusCode).toBe(204)
-// })
+
+
+// DELETE - Eliminar un usuario existente
+test("DELETE -> BASE_URL/:id, should return statusCode 204", async () => {
+    const res = await request(app)
+        .delete(`${BASE_URL}/${userId}`)
+        .send()
+        .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.statusCode).toBe(204)
+})

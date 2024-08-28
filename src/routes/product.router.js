13cef7901919c5e43967cfaae5e5a04b49548e7e
Diego Gomez <diegofelipegogo03@gmail.com>
2024-08-27 23:58:56 -0500

    Update product router

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,4 +1,4 @@
-const { getAll, create, getOne, remove, update } = require('../controllers/product.controllers');
+const { getAll, create, getOne, remove, update, setImage } = require('../controllers/product.controllers');
 const express = require('express');
 const { verifyJwt } = require('../utils/verifyJWT');
 
@@ -6,11 +6,15 @@ const routerProduct = express.Router();
 
 routerProduct.route('/')
     .get(getAll)
-    .post(verifyJwt, create);
+    .post(verifyJwt, create); //🔐
+
+routerProduct.route('/:id/images') //🔐
+    .post(verifyJwt, setImage)
 
 routerProduct.route('/:id')
     .get(getOne)
-    .delete(verifyJwt, remove)
-    .put(verifyJwt, update);
+    .delete(verifyJwt, remove) //🔐
+    .put(verifyJwt, update); //🔐
+
 
 module.exports = routerProduct;
